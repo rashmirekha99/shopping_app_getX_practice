68c0761895bb6deae0d04b4be81e7960de6e5e1f
rashmirekha99 <kothalawalarr.19@uom.lk>
2025-01-31 22:30:47 +0530

    refactor

--- lib/navigators.dart
+++ lib/navigators.dart
@@ -13,16 +13,4 @@ final GlobalKey<NavigatorState> bottomNavigationKey =
     GlobalKey<NavigatorState>(debugLabel: 'bottom');
 
 
-  // static var client = http.Client();
-  // static Future<List<Product>?> fetchProducts() async {
-  //   var response =
-  //       await client.get(Uri.parse('https://fakestoreapi.com/products'));
 
-  //   if (response.statusCode == 200) {
-  //     var jsonString = response.body;
-
-  //     return productFromJson(jsonString);
-  //   } else {
-  //     return null;
-  //   }
-  // }
